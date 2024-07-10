491dc594162d0b52040eb3df0b5f53757105762d
Ibrahim Hossain <ibrahimemon942@gmail.com>
2024-07-10 23:07:03 +0600

    refresh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
 
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import { useState } from 'react';
+import RefrshHandler from './RefrshHandler';
 
 function App(){
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/login" />
+  }
+
   return(
     <div className='App'>
+       <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
       <Route path='/' element={<Navigate to="/login"/>} />
         <Route path='/login' element={<Login/>} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/home' element={<Home/>} />
+        <Route path='/home' element={<PrivateRoute element={<Home />} />} />
       </Routes>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { handleSuccess } from '../utils';
+import { ToastContainer } from 'react-toastify';
+function Home() {
+    const [loggedInUser, setLoggedInUser] = useState('');
+    const navigate = useNavigate();
+    useEffect(() => {
+        setLoggedInUser(localStorage.getItem('loggedInUser'))
+    }, [])
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const handleLogout = (e) => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('loggedInUser');
+        handleSuccess('User Loggedout');
+        setTimeout(() => {
+            navigate('/login');
+        }, 1000)
+    }
+
+   
+
+    return (
+      
+      
+            <div>
+              <h1>{loggedInUser}</h1>
+              <button onClick={handleLogout}>Logout</button>
+              <ToastContainer />
+            </div>
+        
+    )
 }
 
 export default Home
